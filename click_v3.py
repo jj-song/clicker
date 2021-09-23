d9dcd8604ff8f099d3b60685d002a5ade80b4708
Jay Song <jihoon.jay.song@gmail.com>
2021-09-22 22:09:58 -0400

    converted to python 3 syntax and tabs to spaces

--- click_v3.py
+++ click_v3.py
@@ -4,15 +4,14 @@ import sched, time
 from random import randint
 
 #define parameters below
-duration = 8 #this is in hours
+duration = 24 #this is in hours
 
 start_time = datetime.datetime.now()
 stop_time = start_time + datetime.timedelta(hours = duration) #change this to minutes if need it for smaller time frame.
 savedpos = pyautogui.position()
 
-print start_time
-print stop_time
-#print savedpos
+print (start_time)
+print (stop_time)
 
 def move_down(): 
 	pyautogui.moveTo(pyautogui.position()[0], pyautogui.position()[1]+10)
@@ -27,7 +26,7 @@ def press_key():
 	
 def check_for_movement():
 		if pyautogui.position() == savedpos:
-			print "position being changed automatically"
+			#print "position being changed automatically"
 			#print pyautogui.position()
 			#print savedpos
 			#print datetime.datetime.now()
@@ -35,13 +34,13 @@ def check_for_movement():
 			move_up()
 			press_key()
 		else:
-			print "position was changed manually"
+			print ("position was changed manually")
 			#print pyautogui.position()
 			#print savedpos
 			#print datetime.datetime.now()
 	
 
 while datetime.datetime.now() < stop_time:
-	check_for_movement()
-	savedpos = pyautogui.position()
-	time.sleep(randint(60, 120))
+    check_for_movement()
+    savedpos = pyautogui.position()
+    time.sleep(randint(60, 120))
